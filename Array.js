eea58dc9f25eff738d9979d81ef5d0c738993f04
ErikRiswantoS <114674061+ErikRiswantoS@users.noreply.github.com>
2023-09-26 22:23:52 -0700

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,8 @@
+const nama = [];
+nama.push("Erik");
+nama.push("riswanto" ,"Saputra");
+nama[0]="Stevan";
+
+
+console.table(nama);
+console.info(nama.length);
---
+++ Object.js
@@ -0,0 +1,8 @@
+const orang = {};
+
+orang ["Nama"] = "Erik Riswanto";
+orang ["Alamat"] = "Karang Tenggah";
+orang ["umur"] = 22;
+
+delete orang["umur"];
+console.table(orang);
