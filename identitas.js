a7fa9d134866739a058d34098184ea04ce6cf99c
ErikRiswantoS <114674061+ErikRiswantoS@users.noreply.github.com>
2023-10-03 22:36:14 -0700

    Add files via upload

---
+++ identitas.js
@@ -0,0 +1,9 @@
+const identitas = {
+        nim : 20220040226,
+        nama : "Erik Riswanto Saputra",
+        prodi : "teknik informatika",
+        alamat : "Karang Tenggah",
+        ttl : " Sukabumi 03 - 09 - 21",
+    }
+    console.log(`Hallo, nim saya ${identitas.nim} dan nama Saya ${identitas.nama} prodi ${identitas.prodi}`);
+    console.log(`rumah saya di ${identitas.alamat} tempat tanggal lahir Saya ${identitas.ttl}`);
